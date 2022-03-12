1e3cdb9831878f096ffac7a2ecd3f7ba30620ecc
Niklas Mollenhauer <nikeee@outlook.com>
2022-03-12 17:18:40 +0100

    Fork setup-pandoc from r-lib/actions

---
+++ README.md
@@ -0,0 +1,29 @@
+# setup-pandoc
+
+[![RStudio community](https://img.shields.io/badge/community-github--actions-blue?style=social&logo=rstudio&logoColor=75AADB)](https://community.rstudio.com/new-topic?category=Package%20development&tags=github-actions)
+
+This action sets up pandoc and pandoc-citeproc by:
+
+- downloading and caching a relased version of pandoc by version and adding it to the PATH
+
+# Usage
+
+See [action.yml](action.yml)
+
+Basic:
+```yaml
+steps:
+- uses: actions/checkout@master
+- uses: r-lib/actions/setup-pandoc@v1
+  with:
+    pandoc-version: '2.7.3' # The pandoc version to download (if necessary) and use.
+- run: echo "# Test" | pandoc -t html
+```
+
+# License
+
+The scripts and documentation in this project are released under the [MIT License](LICENSE)
+
+# Contributions
+
+Contributions are welcome!  See [Contributor's Guide](docs/contributors.md)
---
+++ src/setup-pandoc.ts
@@ -0,0 +1,152 @@
+let tempDirectory = process.env["RUNNER_TEMP"] || "";
+
+import * as core from "@actions/core";
+import * as exec from "@actions/exec";
+import * as io from "@actions/io";
+import * as tc from "@actions/tool-cache";
+import * as path from "path";
+import * as util from "util";
+import * as fs from "fs";
+import { compare } from 'compare-versions';
+
+const IS_WINDOWS = process.platform === "win32";
+const IS_MAC = process.platform === "darwin";
+
+if (!tempDirectory) {
+  let baseLocation;
+  if (IS_WINDOWS) {
+    // On windows use the USERPROFILE env variable
+    baseLocation = process.env["USERPROFILE"] || "C:\\";
+  } else {
+    if (IS_MAC) {
+      baseLocation = "/Users";
+    } else {
+      baseLocation = "/home";
+    }
+  }
+  tempDirectory = path.join(baseLocation, "actions", "temp");
+}
+
+async function run() {
+  try {
+    let pandocVersion = core.getInput("pandoc-version");
+    core.debug(`got pandoc-version ${pandocVersion}`);
+    await getPandoc(pandocVersion);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+export async function getPandoc(version: string) {
+  if (IS_WINDOWS) {
+    installPandocWindows(version);
+  } else if (IS_MAC) {
+    installPandocMac(version);
+  } else {
+    installPandocLinux(version);
+  }
+}
+
+async function installPandocMac(version: string) {
+  const fileName = util.format("pandoc-%s-macOS.pkg", version);
+  const downloadUrl = util.format(
+    "https://github.com/jgm/pandoc/releases/download/%s/%s",
+    version,
+    fileName
+  );
+  let downloadPath: string | null = null;
+
+  try {
+    downloadPath = await tc.downloadTool(downloadUrl);
+  } catch (error) {
+    throw `Failed to download Pandoc ${version}: ${error}`;
+  }
+
+  await io.mv(downloadPath, path.join(tempDirectory, fileName));
+
+  exec.exec("sudo installer", [
+    "-allowUntrusted",
+    "-dumplog",
+    "-pkg",
+    path.join(tempDirectory, fileName),
+    "-target",
+    "/"
+  ]);
+}
+
+async function installPandocWindows(version: string) {
+  const fileName = util.format("pandoc-%s-windows-x86_64.zip", version);
+  const downloadUrl = util.format(
+    "https://github.com/jgm/pandoc/releases/download/%s/%s",
+    version,
+    fileName
+  );
+  let downloadPath: string | null = null;
+
+  try {
+    downloadPath = await tc.downloadTool(downloadUrl);
+  } catch (error) {
+    throw `Failed to download Pandoc ${version}: ${error}`;
+  }
+
+  //
+  // Extract
+  //
+  let extPath: string = tempDirectory;
+  if (!extPath) {
+    throw new Error("Temp directory not set");
+  }
+
+  extPath = await tc.extractZip(downloadPath);
+
+  const toolPath = await tc.cacheDir(extPath, "pandoc", version);
+
+  // It extracts to this folder
+  const toolRoot = path.join(toolPath, pandocSubdir(version));
+
+  core.addPath(toolRoot);
+}
+
+function pandocSubdir(version: string) {
+  if (compare(version, "2.9.2", ">=")) {
+    return util.format("pandoc-%s", version);
+  }
+
+  if (compare(version, "2.9.1", "=")) {
+    return "";
+  }
+
+  return util.format("pandoc-%s-windows-x86_64", version);
+}
+
+async function installPandocLinux(version: string) {
+  const fileName = util.format("pandoc-%s-1-amd64.deb", version);
+  const downloadUrl = util.format(
+    "https://github.com/jgm/pandoc/releases/download/%s/%s",
+    version,
+    fileName
+  );
+  let downloadPath: string | null = null;
+
+  try {
+    downloadPath = await tc.downloadTool(downloadUrl);
+  } catch (error) {
+    throw `Failed to download Pandoc ${version}: ${error}`;
+  }
+
+  await io.mv(downloadPath, path.join(tempDirectory, fileName));
+
+  try {
+    await exec.exec("sudo apt-get", ["install", "-y", "gdebi-core"]);
+    await exec.exec("sudo gdebi", [
+      "--non-interactive",
+      path.join(tempDirectory, fileName)
+    ]);
+  } catch (error) {
+    core.debug(error);
+
+    throw `Failed to install pandoc: ${error}`;
+  }
+}
+
+run();
